151be4321b781bf732244a2a20d8e7b06dcf7c6e
jiangruijian <jrj444@foxmail.com>
2023-10-17 10:53:36 +0800

    add dashboard page

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,22 @@
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  return (
+    <>
+      <div>This is a Dashboard</div>
+      <div>Acitve Store: {store?.name}</div>
+    </>
+  );
+};
+
+export default DashboardPage;
