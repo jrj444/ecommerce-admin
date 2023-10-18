81aad397ae550d1cb4d20a7e12a8d5b5e862af00
jiangruijian <jrj444@foxmail.com>
2023-10-18 23:31:16 +0800

    add billboard apis

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "res.cloundinary.com"
-    ]
-  }
+    domains: ['res.cloudinary.com'],
+  },
 }
 
 module.exports = nextConfig
