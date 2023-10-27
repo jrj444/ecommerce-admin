dac2fdeee4e87ca3d5bc515eb8bc65645cd9d7c4
jiangruijian <jrj444@foxmail.com>
2023-10-27 11:11:17 +0800

    add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { zhCN } from "@clerk/localizations";
 
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,9 +24,15 @@ export default function RootLayout({
     <ClerkProvider localization={zhCN}>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          >
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
